49eb3dc8f37455ca39c1316770b8803afb57a99d
Pierre-Julien Bringer <pierre-julien.bringer@polytechnique.org>
2013-04-08 14:32:49 +0200

    Reload hooks on SIGUSR1

--- kaoz/hooks.py
+++ kaoz/hooks.py
@@ -2,12 +2,14 @@
 # Copyright © 2011-2013 Binet Réseau
 # See the LICENCE file for more informations
 
+from imp import reload
 import logging
-import sys
 from os import listdir, walk
 from os.path import isfile, join, isdir
-from threading import Thread
+from threading import Thread, RLock
 import traceback
+import signal
+import sys
 
 logger = logging.getLogger(__name__)
 
@@ -19,29 +21,46 @@ class Hooks(object):
     The extension modules are loaded with the load_hook_modules method."""
     def __init__(self, config):
         self.directory = config.get('irc', 'hook_directory')
-        sys.path.append(self.directory);
-        self.modules = []
+        if (self.directory):
+            sys.path.append(self.directory);
+        self._lock = RLock()
+        self.modules = set()
+        signal.signal(signal.SIGUSR1, self.reload)
+
+    def reload(self, signum, stack_frame):
+        logger.info("Caught hook reload signal: %s", self.directory)
+        assert signum==signal.SIGUSR1
+        with self._lock:
+            self.modules = set()
+            self.load_hook_modules()
+        logger.info('Done')
 
     def load_hook_modules(self):
         """Loads any module in the hooks directory as hook handlers"""
         if not self.directory:
             return
         if not isdir(self.directory):
-            logger.error("Hooks directory is not a directory: %s" % str(self.directory))
+            logger.error("Hooks directory is not a directory: %s" %
+                str(self.directory))
             return
-        for x in walk(self.directory):
-            for filename in x[2]:
-                if filename.endswith('.py'):
-                    module = filename[:-3]
-                    self.modules.append(module)
-                else:
-                    logger.info("Ignoring %s" % filename)
-        for m in self.modules:
-            try:
-                logger.info("Loading %s" % m)
-                __import__(m)
-            except ImportError:
-                logger.error("Could not load hook: %s" % m)
+        with self._lock:
+            for dir in walk(self.directory):
+                for filename in dir[2]:
+                    if filename.endswith('.py'):
+                        module = filename[:-3]
+                        self.modules.add(module)
+                    else:
+                        logger.info("Ignoring %s" % filename)
+            for m in self.modules:
+                try:
+                    if m in sys.modules:
+                        logger.info("Reloading %s" % m)
+                        reload(sys.modules[m])
+                    else:
+                        logger.info("Loading %s" % m)
+                        __import__(m)
+                except ImportError:
+                    logger.error("Could not load hook: %s" % m)
 
     def _multiplex_method(self, method_name, *args, **kwargs):
         """Calls method_name on all modules that have it.
@@ -58,11 +77,12 @@ class Hooks(object):
                 logger.error('\n'.join(map(quote_line, error_msg.split('\n'))))
 
         ans = False
-        for m in self.modules:
-            mod = sys.modules[m]
-            if hasattr(mod, method_name):
-                ans = True
-                Thread(None, call_method, None, (mod,)+args, kwargs).start()
+        with self._lock:
+            for m in self.modules:
+                mod = sys.modules[m]
+                if hasattr(mod, method_name):
+                    ans = True
+                    Thread(None, call_method, None, (mod,)+args, kwargs).start()
         return ans
 
     # API methods: methods bollow this line are meant to be forwarded to the extensions 
